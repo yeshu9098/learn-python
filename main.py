2f054019174bf6e0b37d8a413e6b02ff8fca8609
yeshu9098 <yeshu9098@gmail.com>
2023-04-26 18:27:21 +0530

    First

---
+++ main.py
@@ -0,0 +1,67 @@
+#print("Hello World!")
+
+#  Variables in python
+
+# name = "Yeeshu"
+# age = 24
+# float_age = 24.0
+# is_adult = True
+# print(name)
+# print(age)
+# print(float_age)
+# print(is_adult)
+
+# name = input("What is your name : ")
+# print("Hello " + name)
+
+# old_age = input("Enter Your Old Age : ")
+# new_age = int(old_age) + 2
+# print(new_age)
+
+# first = input("Enter First No. : ")
+# second = input("Enter Second No. : ")
+
+# sum = int(first) + int(second)
+
+# print(sum)
+
+# name = "Yeeshu Sharma"
+# print(name.lower())
+# print(name.upper())
+# print(name.find('h'))
+# print(name.replace('Yeeshu', 'Yeshu'))
+# print(name.replace("Y", "I"))
+# print('Y' in name)
+
+# Operators
+
+# print(5 + 2)
+# print(5 - 2)
+# print(5 * 2)
+# print(5 / 2)
+# print(5 // 2)
+# print('Output ' + str(5 ** 2))
+
+# i = 5
+# i = i + 2
+# i += 2
+# print(i)
+
+# Conditional Operators
+# print(3 > 2)
+
+
+# Logical Operators
+
+# print(2 > 3 or 3 > 2)
+# print(2 > 3 and 3 > 2)
+# print(not 4 > 3)
+
+# age = 18
+
+# if age > 18:
+#     print("You can vote")
+# elif age == 18:
+#     print("Apply for Voter card")
+# else:
+#     print("You are under age")
